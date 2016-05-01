e5eff7ed630f02c9d92aab2bada1997097c1f52d
Hardika Saputra <hardika.saputra@live.com>
2016-05-02 05:13:23 +0700

    bismillah

--- common/models/report.js
+++ common/models/report.js
@@ -1,3 +1,45 @@
 module.exports = function(Report) {
 
+	Report.getReport = function(cb) {
+		console.log("njemak")
+		
+	Report.find({limit:100 
+          }, function (err, instance) {
+
+          	var returnvalue = {}
+
+          	returnvalue.type = "FeatureCollection"
+          	var returnvaluearray = []
+          	for (var i = 0;i<instance.length;i++){
+          		var newobject = {}
+          		newobject.type = "Feature"
+          		newobject.properties = {
+          			"title":instance[i].title,
+		            "comment":instance[i].comment,
+		            "picture":instance[i].picture,
+		            "category":instance[i].category
+          		}
+          		newobject.geometry = instance[i].location
+          		returnvaluearray.push(newobject)
+          	}
+
+          	returnvalue.features = returnvaluearray
+
+							cb(null, returnvalue);
+						});
+	};
+	
+	Report.remoteMethod('getReport', {
+		http : {
+			path : '/getReport',
+			verb : 'get',
+			source : 'query'
+		},
+
+		returns :  {
+			arg : 'result',
+			type : 'object'
+		}, 
+	});
+
 };
