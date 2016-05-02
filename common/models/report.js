7de560820332aa69d48bff59dfc027549c16acfe
Hardika Saputra <hardika.saputra@live.com>
2016-05-02 15:42:41 +0700

    selese dulu

--- common/models/report.js
+++ common/models/report.js
@@ -11,11 +11,24 @@ module.exports = function(Report) {
           	var returnvaluearray = []
           	for (var i = 0;i<instance.length;i++){
           		var newobject = {}
+          		var picturenow;
+
+          		if (instance[i].category == "Sampah"){
+          			picturenow = "sampah.jpg"
+          		}else if (instance[i].category == "Kriminal"){
+          			picturenow = "kriminal.jpg"
+          		}else if (instance[i].category == "Kebakaran"){
+          			picturenow = "kebakaran.jpg"
+          		}else if (instance[i].category == "Macet"){
+          			picturenow = "macet.jpg"
+          		}else{
+          			picturenow = "kecelakaan.jpg"
+          		}
           		newobject.type = "Feature"
           		newobject.properties = {
           			"title":instance[i].title,
 		            "comment":instance[i].comment,
-		            "picture":instance[i].picture,
+		            "picture":picturenow,
 		            "category":instance[i].category
           		}
           		newobject.geometry = instance[i].location
